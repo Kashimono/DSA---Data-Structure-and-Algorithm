bee7fb4396320a4602984316cb9e9adceed1b5d6
Kashimono <kashimono443@gmail.com>
2026-09-16 18:13:58 +0800

    test 5

--- DIT projects/src/FibonacciCalculator.java
+++ DIT projects/src/FibonacciCalculator.java
@@ -7,3 +7,18 @@ public class FibonacciCalculator {
         if (n == 0) {
             return 0;
         }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            int result = fibonacci(n - 1) + fibonacci(n - 2);
+            System.out.println("fibonacci(" + n + ") = fibonacci(" + (n - 1) + ") + fibonacci(" + (n - 2) + ") = " + result);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println("Fibonacci of " + number + " is: " + fibonacci(number));
+    }
+}
